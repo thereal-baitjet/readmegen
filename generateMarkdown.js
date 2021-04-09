527ca6312f500c6c73f8c56200df5c9d85f8a21a
Juan Santos <62819163+thereal-baitjet@users.noreply.github.com>
2021-04-09 14:27:00 -0400

    added to GMDjs file

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,20 +1,15 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+function generateMarkdown(response){
+  let badge = "";
+  if(response.license == "MIT"){
+      badge = "![GitHub license](https://img.shields.io/github/license/Naereen/StrapDown.js.svg)"
+  }else if (response.license == "APACHE 2.0"){
+      badge = "![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)"
+  }else if (response.license == "GPL 3.0"){
+      badge = "![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)"
+  }else if (response.license == "BSD 3"){
+      badge = "![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)"
+  }
 }
 
 module.exports = generateMarkdown;
