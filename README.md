cc0c85b9c57c088b0a65bd5e543a741bf2b8476c
Juan Santos <62819163+thereal-baitjet@users.noreply.github.com>
2021-04-10 15:16:04 -0400

    readded the readme, added questions

--- README.md
+++ README.md
@@ -1,30 +1,35 @@
-# readmegen  
-this generator will make a readme file of off questions in a prompt
-## Table of Contents:
-* [Installation](#installation)
-* [Usage](#usage)
-* [License](#license)
-* [Contributing](#contributing)
-* [Tests](#tests)
-* [Questions](#questions)
-# Installation:
-In order to install the necessary dependencies, open the console and run the following:
-```npm i inquirer```
-# Usage:
-the generator will overwrite the file
-![screenshot](https://github.com/thereal-baitjet/readmegen/blob/main/generated%20readme.png)
-view video on how to use my readme gen https://youtu.be/O9OIryzAHBQ
-# License:
-This project is licensed under:
-undefined
-# Contributing:
-you can contact me via email
-# Tests:
-In order to test open the console and run the following:
-```npm run test```
-# Questions:
-If you have any questions contact me on [GitHub](https://github.com/thereal-baitjet) or contact 
-Juan Santos at baitjet@gmail.com
-![picture](https://github.com/thereal-baitjet.png?size=80)
-  
-## Credits: https://www.youtube.com/channel/UCcs3EOl5uqBpDQ5vs87X55w 
+# readmegen  ![GitHub license](https://img.shields.io/github/license/Naereen/StrapDown.js.svg)
+  this generator will make a readme file of off questions in a prompt
+  ## Table of Contents:
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  * [Credits](#credits)
+  ### Installation:
+  In order to install the necessary dependencies, open the console and run the following:
+  ```npm install```
+  ### Usage:
+  the generator will overwrite the file
+  ### License:
+  This project is licensed under:
+  MIT
+  ### Contributing:
+  contact me via email
+  ### Tests:
+  In order to test open the console and run the following:
+  ```npm run test```
+  ### Questions:
+  If you have any questions contact me on [GitHub](https://github.com/thereal-baitjet) or contact 
+  Juan Santos at baitjet@gmail.com
+  ![picture](https://github.com/thereal-baitjet.png?size=80)
+   ### Credits 
+   https://www.youtube.com/channel/UCcs3EOl5uqBpDQ5vs87X55w Josh Glugatch
+   ### badges
+  [![LinkedIn Badge](https://img.shields.io/badge/LinkedIn-Profile-informational?style=flat&logo=linkedin&logoColor=red&color=0D76A8)](https://www.linkedin.com/in/juan-santos-8380b0186/)
+
+
+  ![twitterbadge](https://img.shields.io/twitter/url?logoColor=red&style=social&url=https%3A%2F%2Ftwitter.com%2FBaitjet4)
+   
--- index.js
+++ index.js
@@ -2,16 +2,15 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const Choices = require("inquirer/lib/objects/choices");
-const { report } = require("process");
-const util = require("util")
-// usuing libraries//
+const util = require("util");
+//add library
 
 const writeFileAsync = util.promisify(fs.writeFile)
+//pulls librazy and writes file 
 
-
-function promptUser(){
+function promptUser() {
   return inquirer.prompt([
-    //prompt which initializes questions in inode
+    //prompt with questions
     {
       type: "input",
       name: "author",
@@ -39,7 +38,7 @@ function promptUser(){
     },
     {
       type: "list",
-      name: "License",
+      name: "license",
       message: "what kind of license should your project have?",
       choices: ["MIT", "Apache 2.0", "GPL 3.0", "BSD 3", "none"]
     },
@@ -68,60 +67,74 @@ function promptUser(){
       name: "contribute",
       message: "what does the user need to know to contributre to the repo"
     },
+    {
+      type: "input",
+      name: "credits",
+      message: "did anyone contributre to this repo leave a link to his/her presence and thier name"
+    },
   ])
 }
 //list of questions
 
 
-function generateMarkdown(response){
+function generateMD(response) {
   let badge = "";
-  if(response.license == "MIT"){
-      badge = "![GitHub license](https://img.shields.io/github/license/Naereen/StrapDown.js.svg)"
-  }else if (response.license == "APACHE 2.0"){
-      badge = "![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)"
-  }else if (response.license == "GPL 3.0"){
-      badge = "![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)"
-  }else if (response.license == "BSD 3"){
-      badge = "![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)"
+  if (response.license == "MIT") {
+    badge = "![GitHub license](https://img.shields.io/github/license/Naereen/StrapDown.js.svg)"
+  } else if (response.license == "APACHE 2.0") {
+    badge = "![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)"
+  } else if (response.license == "GPL 3.0") {
+    badge = "![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)"
+  } else if (response.license == "BSD 3") {
+    badge = "![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)"
   }
-  
-  
+  //generates badge depending on chice of list of license
 
-return`# ${response.title}  ${badge}
-${response.description}
-## Table of Contents:
-* [Installation](#installation)
-* [Usage](#usage)
-* [License](#license)
-* [Contributing](#contributing)
-* [Tests](#tests)
-* [Questions](#questions)
-### Installation:
-In order to install the necessary dependencies, open the console and run the following:
-\`\`\`${response.installation}\`\`\`
-### Usage:
-${response.usage}
-### License:
-This project is licensed under:
-${response.license}
-### Contributing:
-${response.contribute}
-### Tests:
-In order to test open the console and run the following:
-\`\`\`${response.tests}\`\`\`
-### Questions:
-If you have any questions contact me on [GitHub](https://github.com/${response.username}) or contact 
-${response.author} at ${response.email}
-![picture](https://github.com/${response.username}.png?size=80)
-  
-`
+
+  return `# ${response.title}  ${badge}
+  ${response.description}
+  ## Table of Contents:
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  * [Credits](#credits)
+  ### Installation:
+  In order to install the necessary dependencies, open the console and run the following:
+  \`\`\`${response.installation}\`\`\`
+  ### Usage:
+  ${response.usage}
+  ### License:
+  This project is licensed under:
+  ${response.license}
+  ### Contributing:
+  ${response.contribute}
+  ### Tests:
+  In order to test open the console and run the following:
+  \`\`\`${response.tests}\`\`\`
+  ### Questions:
+  If you have any questions contact me on [GitHub](https://github.com/${response.username}) or contact 
+  ${response.author} at ${response.email}
+  ![picture](https://github.com/${response.username}.png?size=80)
+   ### Credits 
+   ${response.credits}
+   ### badges
+  [![LinkedIn Badge](https://img.shields.io/badge/LinkedIn-Profile-informational?style=flat&logo=linkedin&logoColor=red&color=0D76A8)](https://www.linkedin.com/in/juan-santos-8380b0186/)
+
+
+  ![twitterbadge](https://img.shields.io/twitter/url?logoColor=red&style=social&url=https%3A%2F%2Ftwitter.com%2FBaitjet4)
+   `
+  //organizes the readme text with response specific prompt questions input and enters it into the resulting string
 }
 
-promptUser().then(function(response){
-  const markdown = generateMarkdown(response);
+promptUser().then(function (response) {
+  const markdown = generateMD(response);
   return writeFileAsync("README.md", markdown);
 }).then(function () {
-      console.log("Generating README.md ...");
-  }).catch(function(err){
+  console.log("README.md was sucessfully created...");
+
+}).catch(function (err) {
   console.log(err)
 })
